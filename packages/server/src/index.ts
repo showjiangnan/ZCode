7abed3fe03d1959bc012de3a93526c2017ad883e
showjiangnan <127078314+showjiangnan@users.noreply.github.com>
2026-09-22 11:51:33 +0900

    feat(server): export app server route registration

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,4 @@
 export { createHttpServer } from "./http.js";
 export { serverAppRuntime, ServerAppRuntime } from "./appServerRuntime.js";
 export { getAppServerStatus } from "./appServerStatus.js";
+export { registerAppServerRoutes } from "./appServerRoutes.js";
