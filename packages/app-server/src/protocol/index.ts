d9c8cdf3315ea9023ca49a96fc89e99bf7103bfc
showjiangnan <127078314+showjiangnan@users.noreply.github.com>
2026-09-22 10:57:46 +0900

    feat: export app server command registry

--- packages/app-server/src/protocol/index.ts
+++ packages/app-server/src/protocol/index.ts
@@ -2,3 +2,4 @@ export * from "./app-server-protocol.js";
 export * from "./request-handler.js";
 export * from "./websocket-adapter.js";
 export * from "./events-stream.js";
+export * from "./commands.js";
